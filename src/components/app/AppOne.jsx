0b5d2cdd0035cc6a637ad93bd5aa877914231f89
evasyukov <egor.vasyukov1@gmail.com>
2024-02-02 12:12:05 +0700

    create page with app one

---
+++ src/components/app/AppOne.jsx
@@ -0,0 +1,12 @@
+export default function AppOne() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>0</h1>
+        <button className="minus">- Минус</button>
+        <button className="plus">Плюс +</button>
+      </div>
+    </div>
+  );
+}
