f5729ed1897f20e40108edb8a801d04be8b2d7b3
evasyukov <egor.vasyukov1@gmail.com>
2024-02-06 12:52:59 +0700

    add array with questions

--- src/components/app/AppThree.jsx
+++ src/components/app/AppThree.jsx
@@ -1,5 +1,25 @@
+const questons = [
+  {
+    title: "React - это?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    corrent: 0,
+  },
+  {
+    title: "Компонент - это?",
+    variants: ["приложение", "часть страницы", "не знаю"],
+    corrent: 1,
+  },
+  {
+    title: ".jsx - это?",
+    variants: [
+      "функция",
+      "библиотека для HTML",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    corrent: 2,
+  },
+]
+
 export default function AppThree() {
-    return (
-        <h3>Опросник</h3>
-    )
+  return <h3>Опросник</h3>
 }
