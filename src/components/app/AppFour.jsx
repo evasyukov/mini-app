c3741afe2d05c41781dc7688f6a796afb7dc947d
evasyukov <egor.vasyukov1@gmail.com>
2024-02-13 11:38:22 +0700

    add page layout for calculator

--- src/components/app/AppFour.jsx
+++ src/components/app/AppFour.jsx
@@ -1,3 +1,45 @@
+import { useState } from "react"
+import Button from "../Button"
+
+const calcButton = [
+  { value: "1" },
+  { value: "2" },
+  { value: "3" },
+  { value: "4" },
+  { value: "5" },
+  { value: "6" },
+  { value: "7" },
+  { value: "8" },
+  { value: "9" },
+  { value: "0" },
+  { value: "=" },
+  { value: "+" },
+  { value: "-" },
+  { value: "*" },
+  { value: "/" },
+]
+
 export default function AppFour() {
-    
+  const [count, setCount] = useState(0)
+
+  function numberInput() {}
+
+  return (
+    <>
+      <h3>калькулятор</h3>
+      <input
+        style={{ marginBottom: "1rem" }}
+        defaultValue={count}
+        type="text"
+        id="calc"
+      />
+      <div>
+        {calcButton.map((item) => (
+          <Button onClick={() => numberInput(item.value)} key={item}>
+            {item.value}
+          </Button>
+        ))}
+      </div>
+    </>
+  )
 }
